d642419edbe3daff67a1d3dede4da4deae301870
bmoxey <bmoxey@gmail.com>
2023-08-05 19:54:29 +1000

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Brett Moxey on 5/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
